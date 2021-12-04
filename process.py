676fd8a35d5ae73d3f1014ac8891ddbc1efcd91b
Omar Abdullah <Malfucious97@gmail.com>
2021-12-04 00:29:54 -0600

    Finished

--- process.py
+++ process.py
@@ -3,7 +3,7 @@ log_file = open("um-server-01.txt") # log_files is saved as a variable and used 
 
 def sales_reports(log_file): # function with the name sales_reports with the argument log_file
     for line in log_file: # for in loop that iterates through the log_file, saved to the variable "line"
-        line = line.rstrip() # line.rstrip will return a copy of the string by removing trailing characters specified as argument 
+        line = line.rstrip() # line.rstrip will return a copy of the string by removing trailing characters on in the "line" variable
         day = line[0:3] # day variable will look at the log_file and goes what whatever is specificed in the index, in this case it'll start at 0 at the index and goes 3 over
         if day == "Mon": # If day equals to Mon, the console log will print the line, which will show the log_file since its saved to "line"
             print(line) # Will print the line or any variable stored
